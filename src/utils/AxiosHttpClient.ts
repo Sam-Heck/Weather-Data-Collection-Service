84aef8a1b67da35fd383f15cc4e2510dec436717
Sam Heck <heck.sam@gmail.com>
2024-12-31 19:20:40 -0800

    create HttpClient interface and AxiosHttpClient class

---
+++ src/utils/AxiosHttpClient.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { HttpClient } from './HttpClient';
+
+export class AxiosHttpClient implements HttpClient {
+    async get(url: string, config?: any): Promise<any> {
+        try {
+            const response = await axios.get(url, config);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Http request failed: ${error}`);
+        }
+    }
+}
