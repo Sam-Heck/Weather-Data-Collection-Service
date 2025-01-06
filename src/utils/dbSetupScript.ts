6982ff51637ec4e2afcee425563429e5e37fe233
Sam Heck <heck.sam@gmail.com>
2025-01-06 15:00:52 -0800

    add table creation for current and forecast weather

--- src/utils/dbSetupScript.ts
+++ src/utils/dbSetupScript.ts
@@ -6,17 +6,52 @@ async function setupDatabase() {
 
         await pool.query('USE weather_data_collection_service')
 
-        await pool.query(`CREATE TABLE IF NOT EXISTS cities (
+        await pool.query(`CREATE TABLE IF NOT EXISTS city (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(100) NOT NULL,
-                country VARCHAR(50) NOT NULL,
+                country CHAR(2),
                 lat DOUBLE NOT NULL,
                 lon DOUBLE NOT NULL,
                 timezone INT
             )
         `)
 
-        // add current weather and forecast weather tables
+        await pool.query(`CREATE TABLE IF NOT EXISTS current_weather (
+                city_id INT NOT NULL,
+                forecast_time INT NOT NULL,
+                weather_condition VARCHAR(50),
+                condition_desc VARCHAR(100),
+                temp FLOAT,
+                feels_like FLOAT,
+                pressure SMALLINT UNSIGNED,
+                humidity TINYINT UNSIGNED,
+                visibility INT,
+                wind_speed FLOAT,
+                wind_deg SMALLINT UNSIGNED,
+                clouds TINYINT UNSIGNED,
+                PRIMARY KEY (city_id, forecast_time),
+                FOREIGN KEY (city_id) REFERENCES city(id)
+            )
+        `)
+
+        await pool.query(`CREATE TABLE IF NOT EXISTS forecast_weather (
+                city_id INT NOT NULL,
+                forecast_time INT NOT NULL,
+                weather_condition VARCHAR(50),
+                condition_desc VARCHAR(100),
+                temp FLOAT,
+                feels_like FLOAT,
+                pressure SMALLINT UNSIGNED,
+                humidity TINYINT UNSIGNED,
+                visibility INT,
+                wind_speed FLOAT,
+                wind_deg SMALLINT UNSIGNED,
+                clouds TINYINT UNSIGNED,
+                precipitation FLOAT,
+                PRIMARY KEY (city_id, forecast_time),
+                FOREIGN KEY (city_id) REFERENCES city(id)
+            )
+        `)
     } catch (error) {
         // add error handling
     }
