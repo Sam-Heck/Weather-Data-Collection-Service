2b47751e58c19045d22053fa854421a64a7835ca
Sam Heck <heck.sam@gmail.com>
2025-01-06 17:20:14 -0800

    add cities file for static city data

---
+++ src/data/cities.ts
@@ -0,0 +1,13 @@
+interface City {
+    name: string;
+    lat: number;
+    lon: number;
+}
+
+export const cities: City[] = [
+    {name: 'Seattle', lat: 47.608013, lon: -122.335167},
+    {name: 'Tokyo', lat: 35.6764, lon: 139.6500},
+    {name: 'London', lat: 51.5072, lon: -0.118092},
+    {name: 'Sydney', lat: -33.865143, lon: 151.2099},
+    {name: 'Hong Kong', lat: 22.302711, lon: 114.177216}
+]
