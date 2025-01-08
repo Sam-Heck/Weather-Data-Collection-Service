4ec1609f24b558eec1d954ec646df181b3c3858b
Sam Heck <heck.sam@gmail.com>
2025-01-08 13:55:39 -0800

    declare saveCity in weatherRepository.

--- src/repository/WeatherRepository.ts
+++ src/repository/WeatherRepository.ts
@@ -1,12 +1,30 @@
 import { pool } from '../utils/MySqlClient.js'
+import { CurrentWeatherDbSchema } from '../models/CurrentWeatherModel.js'
+import { FiveDayForecastDbSchema } from '../models/FiveDayForecastModel.js'
 
 export class weatherRepository {
 
-    async saveCurrentWeather() {
-        // pass in data object as an arg to save
+    async saveCity(): Promise<void> {
+        try {
+            // save city data to db. need to define a city interface in models.
+        } catch(error) {
+
+        }
     }
 
-    async saveForecastWeather() {
-        // pass in data object as an arg to save
+    async saveCurrentWeather(weatherData: CurrentWeatherDbSchema): Promise<void> {
+        try {
+            // if record with current city and dt exists: update fields if different, otherwise add new record
+        } catch(error) {
+            // to do: finish error handling
+        }
+    }
+
+    async saveForecastWeather(forecastData: FiveDayForecastDbSchema[]): Promise<void> {
+        try {
+            // 
+        } catch(error) {
+            // to do: finish error handling
+        }
     }
 }
