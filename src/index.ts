f245aaa849fff9f63570b03b60052c342bbfb2d1
Sam Heck <heck.sam@gmail.com>
2025-01-07 16:49:19 -0800

    begin structuring index.ts to setup database and start app. create initial weatherRepository class layout.

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,23 @@
 import { WeatherService } from "./services/WeatherService.js";
 import { AxiosHttpClient } from "./utils/AxiosHttpClient.js";
 import { setupDatabase } from "./utils/dbSetupScript.js";
+import { cities } from './data/cities.js'
 
+const axiosClient = new AxiosHttpClient();
+const weatherService = new WeatherService(axiosClient);
 
 async function main() {
     try {
         // Initialize database
         await setupDatabase();
-
+        
+        
         console.log('Starting the application...');
-        // test api call as placeholder for application logic
-        await logWeather(47.608013, -122.335167);
+        // call a fetch weather function, which cycles through cities to fetch the weather and save it to the database
+
     } catch(error: any) {
         console.error('Failed to initialize the applicaiton: ', error.message);
         process.exit(1);
-    }
-}
-const axiosClient = new AxiosHttpClient();
-const weatherService = new WeatherService(axiosClient);
-
-
-async function logWeather(lat: number, lon: number) {
-    try {
-        const weatherData = await weatherService.getCurrentWeather(lat, lon);
-        console.log("Current Weather Data:")
-        console.log(weatherData);
-
-        const forecastData = await weatherService.getFiveDayForecast(lat, lon);
-        console.log("Five day forecast:");
-        console.log(forecastData);
-    } catch(error) {
-        console.error(error);
     }
 }
 
---
+++ src/repository/WeatherRepository.ts
@@ -0,0 +1,12 @@
+import { pool } from '../utils/MySqlClient.js'
+
+export class weatherRepository {
+
+    async saveCurrentWeather() {
+        // pass in data object as an arg to save
+    }
+
+    async saveForecastWeather() {
+        // pass in data object as an arg to save
+    }
+}
