de280a7424a2670129ad0a134d4210db70d19a95
Sam Heck <heck.sam@gmail.com>
2025-01-08 21:15:39 -0800

    add config parameter in AxiosHttpClient for readability refactor in WeatherRepository class

--- src/utils/AxiosHttpClient.ts
+++ src/utils/AxiosHttpClient.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { HttpClient } from './HttpClient.js';
 
 export class AxiosHttpClient implements HttpClient {
-    async get(url: string): Promise<any> {
+    async get(url: string, config?: any): Promise<any> {
         try {
-            const response = await axios.get(url);
+            const response = await axios.get(url, config);
             return response.data;
         } catch (error) {
             throw new Error(`Http request failed: ${error}`);
