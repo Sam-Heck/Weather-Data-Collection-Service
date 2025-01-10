b445cac8976ff0a3a7a3cadb2ee7fcfe662fdc0f
Sam Heck <heck.sam@gmail.com>
2025-01-09 20:33:40 -0800

    extract transformWeatherData methods into seperate class to seperate concerns

---
+++ src/services/WeatherDataTransformer.ts
@@ -0,0 +1,49 @@
+import { CurrentWeatherModel } from "../models/CurrentWeatherModel.js";
+import { FiveDayForecastModel } from "../models/FiveDayForecastModel.js";
+import { CurrentWeatherDbSchema, FiveDayForecastDbSchema } from "../models/DbSchemas.js";
+
+export class WeatherDataTransformer {
+    transformCurrentWeatherData(response: CurrentWeatherModel): CurrentWeatherDbSchema {
+        const data: CurrentWeatherDbSchema = {
+            lat: response.coord.lat,
+            lon: response.coord.lon,
+            dt: response.dt,
+            weather_condition: response.weather.main,
+            condition_desc: response.weather.description,
+            temp: response.main.temp,
+            feels_like: response.main.feels_like,
+            pressure: response.main.pressure,
+            humidity: response.main.humidity,
+            visibility: response.visibility,
+            wind_speed: response.wind.speed,
+            wind_deg: response.wind.deg,
+            clouds: response.clouds.all
+        }
+        return data;
+    }
+
+    transformForecastWeatherData(response: FiveDayForecastModel): FiveDayForecastDbSchema[] {
+
+        const data: FiveDayForecastDbSchema[]= [];
+        for (const entry of response.list) {
+            const record: FiveDayForecastDbSchema = {
+                lat: response.city.coord.lat,
+                lon: response.city.coord.lon,
+                dt: entry.dt,
+                weather_condition: entry.weather.main,
+                condition_desc: entry.weather.description,
+                temp: entry.main.temp,
+                feels_like: entry.main.feels_like,
+                pressure: entry.main.pressure,
+                humidity: entry.main.humidity,
+                visibility: entry.visibility,
+                wind_speed: entry.wind.speed,
+                wind_deg: entry.wind.deg,
+                clouds: entry.clouds.all,
+                precipitation: entry.pop
+            }
+            data.push(record);
+        }
+        return data;
+    }
+}
--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -2,7 +2,6 @@ import 'dotenv/config'
 import { HttpClient } from "../utils/HttpClient.js";
 import { CurrentWeatherModel } from "../models/CurrentWeatherModel.js";
 import { FiveDayForecastModel } from "../models/FiveDayForecastModel.js";
-import { CurrentWeatherDbSchema, FiveDayForecastDbSchema } from '../models/DbSchemas.js';
 
 export class WeatherService {
     private currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather";
@@ -10,7 +9,7 @@ export class WeatherService {
 
     constructor(private httpClient: HttpClient) {}
 
-    async getCurrentWeather(lat: number, lon: number): Promise<CurrentWeatherDbSchema> {
+    async getCurrentWeather(lat: number, lon: number): Promise<CurrentWeatherModel> {
         try {
             const response = await this.httpClient.get(this.currentWeatherUrl, {
                 params: {
@@ -20,14 +19,14 @@ export class WeatherService {
                     appid: process.env.WEATHER_API_KEY
                 }
             });
-            return this.transformCurrentWeatherData(response);
+            return response;
         } catch (error) {
             console.error(`Error fetching current weather for latitude: ${lat} and longitude: ${lon}`, error);
             throw new Error("Unable to retrieve current weather.");
         }
     }
 
-    async getFiveDayForecast(lat: number, lon: number): Promise<FiveDayForecastDbSchema[]> {
+    async getFiveDayForecast(lat: number, lon: number): Promise<FiveDayForecastModel> {
         try {
             const response = await this.httpClient.get(this.fiveDayForecastUrl, {
                 params: {
@@ -37,54 +36,10 @@ export class WeatherService {
                     appid: process.env.WEATHER_API_KEY
                 }
             });
-            return this.transformForecastWeatherData(response);
+            return response;
         } catch (error) {
             console.error(`Error fetching five day forecast for latitude: ${lat} and longitude: ${lon}`, error);
             throw new Error("Unable to retrieve five day forecast.");
         }
-    }
-
-    private transformCurrentWeatherData(response: CurrentWeatherModel) {
-        const data: CurrentWeatherDbSchema = {
-            lat: response.coord.lat,
-            lon: response.coord.lon,
-            dt: response.dt,
-            weather_condition: response.weather.main,
-            condition_desc: response.weather.description,
-            temp: response.main.temp,
-            feels_like: response.main.feels_like,
-            pressure: response.main.pressure,
-            humidity: response.main.humidity,
-            visibility: response.visibility,
-            wind_speed: response.wind.speed,
-            wind_deg: response.wind.deg,
-            clouds: response.clouds.all
-        }
-        return data;
-    }
-
-    private transformForecastWeatherData(response: FiveDayForecastModel): FiveDayForecastDbSchema[] {
-
-        const data: FiveDayForecastDbSchema[]= [];
-        for (const entry of response.list) {
-            const record: FiveDayForecastDbSchema = {
-                lat: response.city.coord.lat,
-                lon: response.city.coord.lon,
-                dt: entry.dt,
-                weather_condition: entry.weather.main,
-                condition_desc: entry.weather.description,
-                temp: entry.main.temp,
-                feels_like: entry.main.feels_like,
-                pressure: entry.main.pressure,
-                humidity: entry.main.humidity,
-                visibility: entry.visibility,
-                wind_speed: entry.wind.speed,
-                wind_deg: entry.wind.deg,
-                clouds: entry.clouds.all,
-                precipitation: entry.pop
-            }
-            data.push(record);
-        }
-        return data;
     }
 }
